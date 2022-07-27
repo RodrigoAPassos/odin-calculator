b66f7e5e7a40ae3d360bc4ed17ff65658ca53808
Rodrigo Passos <rodrigoarapassos@gmail.com>
2022-07-27 19:11:18 -0300

    Working calculator, no style yet
    Works but still bugged (lagging after few operations or clearing).
    
    Next try to fix the bug and add style.

--- javascript.js
+++ javascript.js
@@ -1,60 +1,119 @@
 //--Add func--
 function add (a, b) {
-    return a + b;
+    let nA = Number(a);
+    let nB = Number(b);
+    return nA + nB;
 }
 
 //--Subtract func--
 function sub (a, b) {
-    return a - b;
+    let nA = Number(a);
+    let nB = Number(b);
+    return nA - nB;
 }
 
 //--Multiply func--
 function mult (a, b) {
-    return a * b;
+    let nA = Number(a);
+    let nB = Number(b);
+    return nA * nB;
 }
 
 //--Divide func--
 function divide (a, b) {
-    return a / b;
+    let nA = Number(a);
+    let nB = Number(b);
+    return nA / nB;
 }
 
 //--Operate func--
-function operate (operator, a, b) {
+function operate (oprt, a, b) {
     let n = 0;
-    if (operator == "add") {
+    if (oprt == "add") {
         n = add(a, b);
         display(n);
-    }else if (operator == "sub") {
+        enterNum("");
+    }else if (oprt == "sub") {
         n = sub(a, b);
         display(n);
-    }else if (operator == "mult") {
+        enterNum("");
+    }else if (oprt == "mult") {
         n = mult(a, b);
         display(n);
-    }else if (operator == "divide") {
+        enterNum("");
+    }else if (oprt == "divide") {
         n = divide(a, b);
         display(n);
+        enterNum("");
     }
 }
 
-function enterNum () {
+function enterNum (n) {
     const numButton = document.querySelector(".numberButtons");
     numButton.querySelectorAll("button").forEach((button) => {
         button.addEventListener("click", () => {
-        let n = button.className;
-        display(n);
-        operator(dispNumber);
+            n = n + button.className;
+            display(n);
+            clear();
+            operator(n);
+        })
+    });
+}
+
+function enterNum2 (n, a, oprt) {
+    const numButton = document.querySelector(".numberButtons");
+    numButton.querySelectorAll("button").forEach((button) => {
+        button.addEventListener("click", () => {
+            n = n + button.className;
+            display(n);
+            cleared();
+            equal(a, oprt, n);
         })
     });
 }
 
 function display(n) {
     let disp = document.querySelector(".display");
-    disp.innerHTML = disp.innerHTML + n;
-    let dispNumber = disp.innerHTML;
-    return dispNumber;
+    if (n == "") {
+        disp.innerHTML = "0";
+    }else disp.innerHTML = n;
 }
 
-function operator(dispNumber) {
-    
+function operator(n) {
+    let opButtons = document.querySelector(".operatorButtons");
+    opButtons.querySelectorAll("button").forEach((button) =>{
+        button.addEventListener("click", () =>{
+            let oprt = button.className;
+            let a = n;
+            enterNum2("", a, oprt);
+        })
+    });
 }
-enterNum();
+
+function equal(a, oprt, n) {
+    let other = document.querySelector(".others");
+    other.querySelector(".equals").addEventListener("click", () => {
+        let b = n;
+        operate(oprt, a, b);
+    })
+}
+
+function clear() {
+    document.querySelector(".clear").addEventListener("click", () => {
+        display("");
+        enterNum("");
+    })
+}
+
+function cleared() {
+    document.querySelector(".clear").addEventListener("click", () => {
+        enterNum2("");
+    })
+}
+
+function start () {
+    display("");
+    enterNum("");
+}
+
+start();
