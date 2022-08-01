a81eeafe6590235ea739751cbdd48372282ef03a
Rodrigo Passos <rodrigoarapassos@gmail.com>
2022-08-01 17:47:45 -0300

    Finish styling, add keyboard entry.
    Add hover effect on all keys, add some color and fonts to the calculator. Also
    add an error message in case of dividing by zero.
    
    Finish the project by now.

--- javascript.js
+++ javascript.js
@@ -28,12 +28,57 @@ function getNumber() {
     const numButton = document.querySelector(".numberButtons");
     numButton.querySelectorAll("button").forEach((button) => {
         button.addEventListener("click", () => {
-            if (dispValue == "0") {
+            if (dispValue == "" || dispValue == "0" || dispValue == "Don't") {
                 dispValue = button.className;
+                //disable dot after 1st dot
+                if (dispValue.includes(".")) {
+                    numButton.querySelector("#dot").disabled = true;
+                }else numButton.querySelector("#dot").disabled = false;
             }else {
                 dispValue = dispValue + button.className;
+                //disable dot after 1st dot
+                if (dispValue.includes(".")) {
+                    numButton.querySelector("#dot").disabled = true;
+                }else numButton.querySelector("#dot").disabled = false;
             }display();
         })
+    })
+    //keyboard option
+    document.addEventListener("keydown", (event) => {
+        if (event.key == "0") {
+            event.preventDefault();
+            numButton.querySelector("#zero").click();
+        }else if (event.key == "1") {
+            event.preventDefault();
+            numButton.querySelector("#one").click();
+        }else if (event.key == "2") {
+            event.preventDefault();
+            numButton.querySelector("#two").click();
+        }else if (event.key == "3") {
+            event.preventDefault();
+            numButton.querySelector("#three").click();
+        }else if (event.key == "4") {
+            event.preventDefault();
+            numButton.querySelector("#four").click();
+        }else if (event.key == "5") {
+            event.preventDefault();
+            numButton.querySelector("#five").click();
+        }else if (event.key == "6") {
+            event.preventDefault();
+            numButton.querySelector("#six").click();
+        }else if (event.key == "7") {
+            event.preventDefault();
+            numButton.querySelector("#seven").click();
+        }else if (event.key == "8") {
+            event.preventDefault();
+            numButton.querySelector("#eight").click();
+        }else if (event.key == "9") {
+            event.preventDefault();
+            numButton.querySelector("#nine").click();
+        }else if (event.key == ",") {
+            event.preventDefault();
+            numButton.querySelector("#dot").click();
+        }
     })
 }
 
@@ -48,16 +93,16 @@ function operate () {
             let btn = button.className;
             if (btn == "add" && operator == null) {
                 operator = btn;
-                dispValue = "0";
+                dispValue = "";
             } else if (btn == "sub" && operator == null) {
                 operator = btn;
-                dispValue = "0";
+                dispValue = "";
             }else if (btn == "mult" && operator == null) {
                 operator = btn;
-                dispValue = "0";
+                dispValue = "";
             }else if (btn == "divide" && operator == null) {
                 operator = btn;
-                dispValue = "0";
+                dispValue = "";
             }else if (btn == "clear" && secondNum == null) {
                 firstNum = null;
                 result = null;
@@ -72,8 +117,32 @@ function operate () {
             }else if (btn == "equals" && operator == "mult") {
                 result = firstNum * secondNum;
             }else if (btn == "equals" && operator == "divide") {
-                result = firstNum / secondNum;
+                if (secondNum == 0) {
+                    result = "Don't";
+                } else result = firstNum / secondNum;
             }display();
         })
+    })
+    //keyboard option
+    document.addEventListener("keydown", (event) => {
+        if (event.key == "+") {
+            event.preventDefault();
+            oprButton.querySelector("#add").click();
+        }else if (event.key == "-") {
+            event.preventDefault();
+            oprButton.querySelector("#sub").click();
+        }else if (event.key == "*") {
+            event.preventDefault();
+            oprButton.querySelector("#mult").click();
+        }else if (event.key == "/") {
+            event.preventDefault();
+            oprButton.querySelector("#divide").click();
+        }else if (event.key == "Enter") {
+            event.preventDefault();
+            oprButton.querySelector("#equals").click();
+        }else if (event.key == "Escape" || event.key == "Backspace") {
+            event.preventDefault();
+            oprButton.querySelector("#clear").click();
+        }
     })
 }
